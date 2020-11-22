b49c833b953d1a7e0b6c85984eae2ea35f49ff09
kris-Gaudel <gaudelkris@gmail.com>
2020-11-22 01:48:17 -0500

    firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,287 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
-void main() {
-  runApp(FinanceApp());
+//void main() async => runApp(MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
-class FinanceApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      title: 'Elevate Finance App',
+      initialRoute: HomePage.id,
+      routes: {
+        HomePage.id: (context) => HomePage(),
+        Login.id: (context) => Login(),
+        Registration.id: (context) => Registration(),
+        Input.id: (context) => Input()
+      },
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-final GoogleSignIn googleSignIn = GoogleSignIn();
-
-class _HomePageState extends State<HomePage> {
+class HomePage extends StatelessWidget {
+  static const String id = 'HOMEPAGE';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFf5fbfd),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.only(top: 120.0),
-              child: Text(
-                'Welcome To Elevate',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Welcome to Elevate Finance',
                 style: TextStyle(
-                    color: Color(0xFF19467d),
                     fontFamily: 'Montserrat',
-                    fontSize: 30.0),
-              ),
-            ),
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.all(100.0),
-                child: FlatButton(
-                  color: Color(0xFF19467d),
-                  onPressed: (){
-                    print('hi');
-                    _signInWithGoogle();
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.all(0.0),
-                    child: Text(
-                      'Sign In With Google',
-                      style: TextStyle(color: Color(0xFFf5fbfd), fontFamily: 'Montserrat', fontSize: 15.0),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
+                    fontSize: 20.0,
+                    color: Color(0xFF19467d)),
+              )
+            ],
+          ),
+          SizedBox(height: 50.0),
+          CustomButton(
+            labelText: 'Login',
+            callBack: () {
+              Navigator.of(context).pushNamed(Login.id);
+            },
+          ),
+          CustomButton(
+            labelText: 'Register',
+            callBack: () {
+              Navigator.of(context).pushNamed(Registration.id);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final VoidCallback callBack;
+  final String labelText;
+  const CustomButton({this.callBack, this.labelText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Material(
+        color: Color(0xFF19467d),
+        elevation: 6.0,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: callBack,
+          minWidth: 200.0,
+          height: 45.0,
+          child: Text(
+            labelText,
+            style:
+                TextStyle(fontFamily: 'Montserrat', color: Color(0xFFf5fbfd)),
+          ),
         ),
       ),
     );
   }
 }
 
-_signInWithGoogle() async{
-  final GoogleSignInAccount googleUser = await googleSignIn.signIn();
-  final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+class Registration extends StatefulWidget {
+  static const String id = "REGISTRATION";
+  @override
+  _RegistrationState createState() => _RegistrationState();
+}
 
-  final AuthCredential credential = GoogleAuthProvider.getCredential(idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
-  final FirebaseUser user = (await firebaseAuth.signInWithCredential(credential)).user;
+class _RegistrationState extends State<Registration> {
+  String email;
+  String password;
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> register() async {
+    User user = (await _auth.createUserWithEmailAndPassword(
+            email: email, password: password))
+        .user;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Input(user: user,),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF19467d),
+        title: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(right: 40.0),
+            child: Text(
+              'Elevate Finance',
+              style: TextStyle(fontFamily: 'Montserrat', fontSize: 20.0),
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Text(
+                  'Register for Elevate',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      fontFamily: 'Montserrat',
+                      color: Color(0xFF19467d),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 40.0),
+          TextField(
+            keyboardType: TextInputType.emailAddress,
+            onChanged: (value) => email = value,
+            decoration: InputDecoration(
+              hintText: 'Email Address',
+              border: const OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(height: 40.0),
+          TextField(
+            autocorrect: false,
+            obscureText: true,
+            onChanged: (value) => password = value,
+            decoration: InputDecoration(
+              hintText: 'Password',
+              border: const OutlineInputBorder(),
+            ),
+          ),
+          CustomButton(labelText: 'Register', callBack: () async{
+            await register();
+          },),
+          SizedBox(height: 100.0),
+        ],
+      ),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  static const String id = "LOGIN";
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  String email;
+  String password;
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> login() async {
+    User user = (await _auth.signInWithEmailAndPassword(
+        email: email, password: password))
+        .user;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Input(user: user,),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF19467d),
+        title: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(right: 40.0),
+            child: Text(
+              'Elevate Finance',
+              style: TextStyle(fontFamily: 'Montserrat', fontSize: 20.0),
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Text(
+                  'Login to Elevate',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontFamily: 'Montserrat',
+                    color: Color(0xFF19467d),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 40.0),
+          TextField(
+            keyboardType: TextInputType.emailAddress,
+            onChanged: (value) => email = value,
+            decoration: InputDecoration(
+              hintText: 'Email Address',
+              border: const OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(height: 40.0),
+          TextField(
+            autocorrect: false,
+            obscureText: true,
+            onChanged: (value) => password = value,
+            decoration: InputDecoration(
+              hintText: 'Password',
+              border: const OutlineInputBorder(),
+            ),
+          ),
+          CustomButton(labelText: 'Login', callBack: () async{
+            await login();
+            },
+          ),
+          SizedBox(height: 100.0),
+        ],
+      ),
+    );
+  }
+}
+
+class Input extends StatefulWidget {
+  static const String id = "INPUT";
+  final User user;
+
+  const Input({Key key, this.user}) : super(key: key);
+  @override
+  _InputState createState() => _InputState();
+}
+
+class _InputState extends State<Input> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
